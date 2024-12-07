f45fdd8ca1abcd68ebb1ab99053fcb905fb03851
Haoran Zhao <167980888+nuo534202@users.noreply.github.com>
2024-12-07 21:26:28 +0800

    Update P3373.cpp

--- Luogu/P3373.cpp
+++ Luogu/P3373.cpp
@@ -12,10 +12,7 @@ struct node { int sum, add, mul; } t[N << 2];
  
 inline int read() {
     int x = 0, f = 1; char c = getchar();
-    while (c < '0' || c > '9') {
-        if (c == '-') f = -1;
-        c = getchar();
-    }
+    while (c < '0' || c > '9') { if (c == '-') f = -1; c = getchar(); }
     while (c >= '0' && c <= '9') x = x * 10 + c - '0', c = getchar();
     return x * f;
 }
