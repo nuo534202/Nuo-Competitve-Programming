5a6d67e97706fb5cad9e0029d7dab4619524ad4d
Haoran Zhao <167980888+nuo534202@users.noreply.github.com>
2024-12-07 21:35:52 +0800

    Update P3374segment_tree.cpp

--- Luogu/P3374segment_tree.cpp
+++ Luogu/P3374segment_tree.cpp
@@ -2,45 +2,56 @@
 using namespace std;
  
 #define int long long
+#define ls (p << 1)
+#define rs (p << 1 | 1)
+#define mid ((l + r) / 2)
  
 const int N = 5e5 + 10;
-int n, m, a[N], t[N], opt, x, y;
+int n, m, a[N];
+struct node { int sum; } t[N << 2];
  
 inline int read() {
     int x = 0, f = 1; char c = getchar();
-    while (c <'0' || c > '9') { if (c == '-') f = -1; c = getchar(); }
-    while (c >= '0' && c <= '9') x = x * 10 + c -'0', c = getchar();
+    while (c < '0' || c > '9') {
+        if (c == '-') f = -1;
+        c = getchar();
+    }
+    while (c >= '0' && c <= '9') x = x * 10 + c - '0', c = getchar();
     return x * f;
 }
  
-int lowbit(int x) { return x & (-x); }
+void pushUp(int p) { t[p].sum = t[ls].sum + t[rs].sum; }
  
-void modify(int x, int k) {
-    while (x <= n) {
-        t[x] += k;
-        x += lowbit(x);
-    }
+void build(int p = 1, int l = 1, int  r = n) {
+    if (l == r) { t[p].sum = a[l]; return; }
+    build(ls, l, mid), build(rs, mid + 1, r);
+    pushUp(p);
 }
-
-int query(int x) {
-    int oup = 0LL;
-    while (x) {
-        oup += t[x];
-        x -= lowbit(x);
-    }
-    return oup;
-} // 表示 [1, x] 的和
+ 
+void modify(int q, int k, int p = 1, int l = 1, int r = n) {
+    if (l == r) { t[p].sum += k; return; }
+    if (q <= mid) modify(q, k, ls, l, mid);
+    if (q > mid) modify(q, k, rs, mid + 1, r);
+    pushUp(p);
+}
+ 
+int query(int ql, int qr, int p = 1, int l = 1, int r = n) {
+    if (ql <= l && r <= qr) return t[p].sum;
+    int ans = 0LL;
+    if (ql <= mid) ans += query(ql, qr, ls, l, mid);
+    if (qr > mid) ans += query(ql, qr, rs, mid + 1, r);
+    return ans; 
+}
  
 signed main() {
     n = read(), m = read();
-    for (int i = 1; i <= n; i++) {
-        a[i] = read();
-        modify(i, a[i]);
-    }
+    for (int i = 1; i <= n; i++) a[i] = read();
+    build();
     while (m--) {
+        int opt, x, y;
         opt = read(), x = read(), y = read();
         if (opt == 1) modify(x, y);
-        if (opt == 2) printf("%lld\n", query(y) - query(x - 1));
+        else printf("%lld\n", query(x, y));
     }
     return 0;
 }
