22342971c3a66a42c9627cb1035c5d0f525a24a8
Haoran Zhao <167980888+nuo534202@users.noreply.github.com>
2024-12-07 21:34:48 +0800

    Create P3374binary_indexed_tree.cpp

---
+++ Luogu/P3374binary_indexed_tree.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+ 
+const int N = 5e5 + 10;
+int n, m, a[N], t[N], opt, x, y;
+ 
+inline int read() {
+    int x = 0, f = 1; char c = getchar();
+    while (c <'0' || c > '9') { if (c == '-') f = -1; c = getchar(); }
+    while (c >= '0' && c <= '9') x = x * 10 + c -'0', c = getchar();
+    return x * f;
+}
+ 
+int lowbit(int x) { return x & (-x); }
+ 
+void modify(int x, int k) {
+    while (x <= n) {
+        t[x] += k;
+        x += lowbit(x);
+    }
+}
+
+int query(int x) {
+    int oup = 0LL;
+    while (x) {
+        oup += t[x];
+        x -= lowbit(x);
+    }
+    return oup;
+} // 表示[1,x]的和
+ 
+signed main() {
+    n = read(), m = read();
+    for (int i = 1; i <= n; i++) {
+        a[i] = read();
+        modify(i, a[i]);
+    }
+    while (m--) {
+        opt = read(), x = read(), y = read();
+        if (opt == 1) modify(x, y);
+        if (opt == 2) printf("%lld\n", query(y) - query(x - 1));
+    }
+    return 0;
+}
