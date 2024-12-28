d44a32f1447cfd898dd2a502d2279ec391783868
Haoran Zhao <167980888+nuo534202@users.noreply.github.com>
2024-12-28 14:11:35 +0800

    Update C.cpp

--- Atcoder/Atcoder Beginner Contest/ABC385/C.cpp
+++ Atcoder/Atcoder Beginner Contest/ABC385/C.cpp
@@ -5,7 +5,7 @@ const int N = 3005;
 int n, h[N], dp[N][N];
 
 int main() {
-    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 	cin >> n;
 	for (int i = 1; i <= n; i++) cin >> h[i];
 	for (int i = 1; i <= n; i++)
@@ -17,8 +17,8 @@ int main() {
 		}
 	}
 	int ans = 0;
-	for (int i = 1; i <= n; i++) 
+	for (int i = 1; i <= n; i++)
 		for (int j = 1; j <= n; j++) ans = max(ans, dp[i][j]);
 	cout << ans << endl;
-    return 0;
+	return 0;
 }
