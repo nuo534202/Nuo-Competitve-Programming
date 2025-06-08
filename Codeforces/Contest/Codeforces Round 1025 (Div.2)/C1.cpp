200bbc64276b341a91d7c1b5569ce9d58b166f8e
nuo534202 <167980888+nuo534202@users.noreply.github.com>
2025-06-08 22:25:41 +0800

    Update C1.cpp

--- Codeforces/Contest/Codeforces Round 1025 (Div.2)/C1.cpp
+++ Codeforces/Contest/Codeforces Round 1025 (Div.2)/C1.cpp
@@ -1,20 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int solve(int i) {
-    int res = 0;
-    while (i) res += (i % 10), i /= 10;
-    return res;
+int ask(const string &op, int y) {
+	if (op == "digit" || op == "!") cout << op << endl;
+	else cout << op << " " << y << endl;
+	cout.flush();
+	int ans;
+	cin >> ans;
+	return ans;
+}
+
+void solve() {
+	int n;
+	cin >> n;
+	ask("digit", 0), ask("digit", 0);
+	ask("add", -8);
+	ask("add", -4);
+	ask("add", -2);
+	ask("add", -1);
+	ask("add", n - 1);
+	ask("!", 0);
 }
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
-	int res = 0;
-    for (int i = 1; i <= 1000000000; i++) {
-        int tmp = solve(i);
-        tmp = solve(tmp);
-        res = max(res, tmp);
-    }
-    cout << res << endl;
+    int T;
+    cin >> T;
+    while (T--) solve();
     return 0;
 }
