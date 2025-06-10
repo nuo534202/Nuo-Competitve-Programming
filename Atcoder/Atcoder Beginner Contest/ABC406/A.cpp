3c993e843d4118dba95ce881c40bfa4a13a8d7dd
nuo534202 <167980888+nuo534202@users.noreply.github.com>
2025-06-10 11:51:26 +0800

    Create A.cpp

---
+++ Atcoder/Atcoder Beginner Contest/ABC406/A.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if (a * 60 + b >= c * 60 + d) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}
