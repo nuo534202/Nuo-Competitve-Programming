17df32926093a750fc681bdd2702240c478f16b6
nuo534202 <167980888+nuo534202@users.noreply.github.com>
2025-08-21 22:05:48 +0800

    Create 1000.cpp

---
+++ HDOJ/1000.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+	int a, b;
+	while (cin >> a >> b) {
+		cout << a + b << endl;
+	}
+    return 0;
+}
