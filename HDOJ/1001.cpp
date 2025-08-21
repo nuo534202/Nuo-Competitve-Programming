04aca1214c610ead587462ba86266faf33fca245
nuo534202 <167980888+nuo534202@users.noreply.github.com>
2025-08-21 22:15:30 +0800

    Create 1001.cpp

---
+++ HDOJ/1001.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+	int n;
+	while (cin >> n) {
+		cout << 1LL * n * (n + 1) / 2 << "\n\n";
+	}
+  return 0;
+}
